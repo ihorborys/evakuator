03b22e207c09a633cfa4fa2883e1fe91e46eae12
Yuriy Borys <iam618384@gmail.com>
2023-04-10 10:13:21 +0300

    Init commit cartoon design

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
-const newGameButton = document.querySelector(".call_now");
+const truck = document.querySelector(".truck");
+const callButton = document.querySelector(".call__button");
+const infoItemH1 = document.querySelector(".header__info h1");
+const infoItemH2 = document.querySelector(".hq");
+const infoItemH3 = document.querySelector(".h24");
+const animationDutation = 0.8;
+const halfAnimationDuration = animationDutation / 2;
 
-function onMouseEntered() {
-    playSound("../assets/sounds/hover-button-sound.mp3");
-    }
+function playTruckSlideIn() {
+    truck.style.animation = `${animationDutation}s ease-out 0s 1 truckSlideInFromLeft`;
+}
 
-function initListeners() {
-    newGameButton.addEventListener("mouseenter", onMouseEntered);
-    }
+function playInfoSlideIn() {
+    infoItemH1.style.visibility = "visible";
+    infoItemH1.style.animation = `${halfAnimationDuration}s ease-out 0s 1 infoSlideInFromTop`;
+    infoItemH2.style.animation = `${halfAnimationDuration}s ease-out ${halfAnimationDuration}s 1 infoSlideInFromTop`;
+    infoItemH3.style.animation = `${halfAnimationDuration}s ease-out ${halfAnimationDuration}s 1 infoSlideInFromTop`;
+}
+
+function playCallButtonSlideIn() {
+    callButton.style.visibility = "visible";
+    callButton.style.animation = `${halfAnimationDuration}s ease-out 0s 1 callButtonSlideInFromBottom`;
+}
+
+truck.onanimationend = () => {
+    playInfoSlideIn();
+};
+
+infoItemH1.onanimationend = () => {
+    infoItemH2.style.visibility = "visible";
+    infoItemH3.style.visibility = "visible";
+};
+
+infoItemH3.onanimationend = () => {
+    playCallButtonSlideIn();
+};
+
+window.onload = () => {
+    playTruckSlideIn();
+};
