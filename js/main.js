275ec6a785fc88b14200b17cedd85c4923c00ac9
Yuriy Borys <iam618384@gmail.com>
2023-05-28 14:19:58 +0300

    Added menu items scroll behaviour

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 import { startHeaderAnimationSequence } from "./home.js";
 import { initSlideshow } from "./slideshow.js";
 
+const gallery = document.querySelector(".gallery");
+const services = document.querySelector(".services");
+const testimonials = document.querySelector(".testimonials");
+const sections = [gallery, services, testimonials];
+
 window.addEventListener("load", () => {
     startHeaderAnimationSequence();
     initSlideshow();
+});
+
+sections.forEach((section) => {
+    const button = document.querySelector("." + section.className + "_btn");
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        section.scrollIntoView({ behavior: "smooth" });
+    });
 });
